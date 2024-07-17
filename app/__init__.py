a9556cf034475836edac18194b5f266c54370c68
improve11 <142506318+improve11@users.noreply.github.com>
2024-07-17 21:19:00 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,21 @@
+import os
+from flask import Flask
+from .database import init_db
+
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = os.urandom(24)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from .models import db
+
+    db.init_app(app)
+    init_db(app)
+
+    with app.app_context():
+        from . import main
+        app.register_blueprint(main.bp)
+
+    return app
 
